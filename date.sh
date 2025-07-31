97f084b33808c29c64ed4c2bfd642440fae93fb0
DandyTheWood <danielmolda1@gmail.com>
2025-07-31 22:31:40 +0200

    Link and content updates

--- date.sh
+++ date.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-git_last_commit_date="$(git log -1 --format=%cd --date=format:'%d.%m.%Y')"
-echo "Git last commit date: $git_last_commit_date"
+current_date="$(date +'%d.%m.%Y')"
+echo "Current date: $current_date"
 
 html_files="$(find . -type f -name "*.html")"
 
-for file in $html_files; do 
-  echo "Inserting date and year into: $file"
-  sed -i "s|<span id=\"git-last-commit-date\">.*</span>|<span id=\"git-last-commit-date\">$git_last_commit_date</span>|g" "$file"
+for file in $html_files; do
+  echo "Updating dates in: $file"
+  sed -i "s|<span id=\"current-date\">.*</span>|<span id=\"current-date\">$current_date</span>|g" "$file"
 done
